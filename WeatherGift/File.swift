9063f4bb888af425a0bd45fe368499c53ac834aa
schmitck <schmitck@bc.edu>
2020-03-09 14:22:00 -0400

    Finished 7.1 and 7.2

---
+++ WeatherGift/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  WeatherGift
+//
+//  Created by Cooper Schmitz on 3/9/20.
+//  Copyright © 2020 Cooper Schmitz. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double 
+}
