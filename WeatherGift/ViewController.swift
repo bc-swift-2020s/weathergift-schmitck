0eb29586f7a1cb1288a30cbc6d0aa371e7d3b0a0
schmitck <schmitck@bc.edu>
2020-03-09 13:48:35 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Cooper Schmitz on 3/9/20.
+//  Copyright © 2020 Cooper Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
