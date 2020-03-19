0d0723ceb277280db56dddb051341d328bddb530
schmitck <schmitck@bc.edu>
2020-03-19 16:52:56 -0400

    with .gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Cooper Schmitz on 3/19/20.
+//  Copyright © 2020 Cooper Schmitz. All rights reserved.
+//
+
+import Foundation
+
+struct APIKey {
+    static let googlePlacesKey = "AIzaSyBhUba6FYj-Go7CGQdCpxhdmBZAWmBx79Y"
+}
