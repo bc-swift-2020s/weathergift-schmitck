ca8c59b1aae46a3742fa9334e083f2422acd5471
schmitck <schmitck@bc.edu>
2020-03-22 14:38:54 -0400

    fixed key

--- WeatherGift/APIKeys.swift
+++ WeatherGift/APIKeys.swift
@@ -1,1 +1,10 @@
+import Foundation
 
+struct APIKey {
+    static let googlePlacesKey = "AIzaSyARoVfwSFjFaGZGsN-NGkrFPEcA6L3qAkM"
+    static let darkSkyKey = "b89d2c519d31b99a062b0e402a2a5688"
+}
+
+struct APIurls{
+    static let darkSkyURL = "https://api.darksky.net/forecast/"
+}
